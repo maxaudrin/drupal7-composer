8c26c1c48d5e0d983761ece6f84cc15680cbedba
Maxime <maxaudrin@gmail.com>
2017-04-06 11:42:30 +0200

    Add drush site-install to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-echo "hello install script";
+echo "Starting install script"
+
+./vendor/bin/drush site-install -y minimal --db-url=$DATABASE_URL --account-pass=admin --site-name="Drupal 7 composer"
+
+echo "Install script finished"
