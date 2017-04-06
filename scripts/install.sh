184ac66fa2af6245354523b89e8f255bcdc0d56a
Maxime <maxaudrin@gmail.com>
2017-04-06 11:54:23 +0200

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@
 echo "Starting install script"
 
 cd web
-./vendor/bin/drush site-install -y minimal --db-url=$DATABASE_URL --account-pass=admin --site-name="Drupal 7 composer"
+vendor/bin/drush site-install -y minimal --db-url=$DATABASE_URL --account-pass=admin --site-name="Drupal 7 composer"
 
 echo "Install script finished"
